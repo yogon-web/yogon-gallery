cdc09b95106d0adb2abdf0b0c3ae45de5bcd46a4
yogon-web <81514642+yogon-web@users.noreply.github.com>
2021-04-10 20:17:38 +0900

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-  $(".fade-img img:not(:first-child)").hide();
-  setInterval(function() {
-    $(".fade-img img:first-child").fadeOut("slow").next("img").fadeIn("slow").end().appendTo(".fade-img");
-  },5000);
-});

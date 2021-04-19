f958314fa16db24bf4728d631e79f30af8e006ed
yogon-web <81514642+yogon-web@users.noreply.github.com>
2021-04-19 22:07:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ $(function(){
     $(".fade-img img:first-child").fadeOut("slow").next("img").fadeIn("slow").end().appendTo(".fade-img");
   },5000);
 });
+
+
+
+
+
+
+
+
+$(function(){
+    $(window).scroll(function (){
+        $('.left-to-right, .down-to-top').each(function(){
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + 150){
+                $(this).addClass('scrollin');
+            }
+        });
+    });
+});

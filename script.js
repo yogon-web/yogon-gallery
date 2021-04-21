9c9e8f2a9d1377058326135f92fe9533eb6ce378
yogon-web <81514642+yogon-web@users.noreply.github.com>
2021-04-21 21:25:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -49,20 +49,3 @@ $(function(){
     $nav.removeClass( open );
   });
 } )(jQuery);
-
-
-
-
-
-
-
-
-$(function () {
-  $(".image__sub__thumb").first().addClass("active"); // 1枚目の小画像をアクティブに変更
-  $('.image__sub__thumb__photo').click(function () { // 小画像がクリックされたらイベント発火
-    image_url = $(this).attr("src"); // クリックされた画像のPATHを取得
-    $(".image__main__photo").attr("src", image_url).hide().fadeIn(); // メイン画像をクリックされた画像に変更
-    $(".image__sub__thumb.active").removeClass("active"); // 1枚目の小画像のアクティブを無効化
-    $(this).parent().addClass("active"); // クリックされた小画像をアクティブに変更
-  });
-});

5d53959cfc0241a1163aa6a5c692166bde818d19
yogon-web <81514642+yogon-web@users.noreply.github.com>
2021-04-22 22:34:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-$(function(){
-  $(".fade-img img:not(:first-child)").hide();
-  setInterval(function() {
-    $(".fade-img img:first-child").fadeOut("slow").next("img").fadeIn("slow").end().appendTo(".fade-img");
-  },5000);
-});
+
 
 
 
